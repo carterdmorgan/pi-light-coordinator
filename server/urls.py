ea86b0eead76f3ba73396cf422705155d442dc79
Carter Morgan <carterdmorgan@gmail.com>
2021-07-27 22:54:49 -0400

    added readme

--- server/urls.py
+++ server/urls.py
@@ -19,9 +19,9 @@ from django.conf.urls import url, include
 
 @api_view(['GET','POST'])
 def hello_world(request):
-        if request.method == 'POST':
-                return Response({"message": "Got some data!", "data": request.data})
-        return Response({"message": "Hello, Carter!"})
+    if request.method == 'POST':
+        return Response({"message": "Got some data!", "data": request.data})
+    return Response({"message": "Hello, Carter!"})
 
 urlpatterns = [
   url(r'^hello/$', hello_world),
